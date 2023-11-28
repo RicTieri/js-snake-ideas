54d2f28e88236a1fc59b8c209e4d524b36cbf267
RicTieri <tieri.riccardo@gmail.com>
2023-11-28 20:29:27 +0100

    added ui adjustment

--- recap-js-snake/main.js
+++ recap-js-snake/main.js
@@ -2,6 +2,7 @@
 const container = document.querySelector('.container');
 let score = 0;
 let direzione = 'right';
+let position;
 let columnsNumber = 20;
 let head = {};
 let body = [];
@@ -11,6 +12,17 @@ let game_run = true;
 
 
 
+// ====================================EVENT_LISTENER=========================================
+
+document.querySelector('button#reset').addEventListener('click', () => {
+    reset();
+    start_game();
+});
+
+document.querySelector('button#start').addEventListener('click', () => {
+    reset();
+    start_game();
+});
 
 document.querySelector('button#pause').addEventListener('click', () => {
     if (game_run) {
@@ -20,59 +32,72 @@ document.querySelector('button#pause').addEventListener('click', () => {
         start_game();
         game_run = true;
     }
-})
+});
 
-document.querySelector('button#reset').addEventListener('click', () => {
-    reset();
-    start_game();
-})
-
-document.querySelector('button#start').addEventListener('click', () => {
-    reset();
-    start_game();
-})
+document.addEventListener('keydown', function (event) {
+    switch (event.code) {
+        case "ArrowLeft":
+            if(direzione !== 'right') direzione = 'left';
+            break;
+        case "ArrowUp":
+            if(direzione !== 'bottom') direzione = 'top'
+            break;
+        case "ArrowDown":
+            if(direzione !== 'top') direzione = 'bottom'
+            break;
+        case "ArrowRight":
+            if(direzione !== 'left') direzione = 'right'
+            break;
+    }
+});
 
 
 
 
 // ===========================================FUNZIONI=========================================
-
+/**
+ * function to reset variables to start point
+ */
 function reset(){
     clearInterval(game);
     container.innerHTML = '';
     container.classList.remove('bg-black');
     createGrid(container, columnsNumber);
+    direzione = 'right';
     food = { point: 0 };
     body = [];
-    let position = Math.floor(Math.random() * (100) + 1);
+    position = Math.floor(Math.random() * (100) + 1);
     head.point = position;
     score = 0;
     document.getElementById('score').innerHTML = score
     game_run = true;
 };
-
+ /**
+  * function to draw game object
+  */
 function start_game() {
     game = setInterval(function () {
-        document.querySelector('#cell-' + head.point).classList.add('active');
+        // draw head
         document.querySelector('#cell-' + head.point).classList.remove('active');
+        head.point = newPosition(direzione, head.point, columnsNumber);
+        document.querySelector('#cell-' + head.point).classList.add('active');
+        // draw body with condition
         body.forEach((element) => {
             document.querySelector('#cell-' + element).classList.remove('active');
-        })
-
+        });
+        
         if (body.length) {
             body.push(head.point);
             body.shift();
+            for (let i = 0; i < body.length - 1; i++) {
+                if(body[i] == head.point) gameOver();
+            };
         };
-
-
+        
         for (let i = 0; i < body.length; i++) {
             document.querySelector('#cell-' + body[i]).classList.add('active');
         };
-
-        head.point = newPosition(direzione, head.point, columnsNumber);
-        document.querySelector('#cell-' + head.point).classList.add('active');
-
-
+        // food interaction
         if (head.point == food.point) {
             document.querySelector('#cell-' + food.point).classList.remove('food')
             body.push(head.point);
@@ -82,16 +107,19 @@ function start_game() {
         };
 
         if (food.point == 0) {
-            placeFood(1, 100, food);
+            placeFood(1, columnsNumber*columnsNumber, food);
             document.querySelector('#cell-' + food.point).classList.add('food');
         };
-
-        
-
     }, 200);
 }
 
-
+/**
+ * function to update grid position based on event listener output
+ * @param {*} direction 
+ * @param {*} position 
+ * @param {*} columns 
+ * @returns 
+ */
 function newPosition(direction, position, columns) {
 
     switch (direction) {
@@ -122,29 +150,11 @@ function newPosition(direction, position, columns) {
 
 };
 
-document.addEventListener('keydown', function (event) {
-    switch (event.code) {
-
-        case "ArrowLeft":
-            if(direzione !== 'right') direzione = 'left';
-            break;
-
-        case "ArrowUp":
-            if(direzione !== 'bottom') direzione = 'top'
-            break;
-
-        case "ArrowDown":
-            if(direzione !== 'top') direzione = 'bottom'
-            break;
-
-        case "ArrowRight":
-            if(direzione !== 'left') direzione = 'right'
-            break;
-
-    }
-
-});
-
+/**
+ * function to draw the basis of the game's box
+ * @param {*} container 
+ * @param {*} columns 
+ */
 function createGrid(container, columns) {
     for (let i = 0; i < columns*columns; i++) {
         let cell = document.createElement('div');
@@ -156,10 +166,19 @@ function createGrid(container, columns) {
     }
 };
 
+/**
+ * function to set random position to food
+ * @param {*} min 
+ * @param {*} max 
+ * @param {*} fruit 
+ */
 function placeFood(min, max, fruit) {
     fruit.point = Math.floor(Math.random() * (max - min)) + min;
 };
 
+/**
+ * function to stop the game
+ */
 function gameOver() {
     container.classList.add('bg-black');
     clearInterval(game);
