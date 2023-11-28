b3089655c4eac2d702d0ba94a02105c87ee597fc
RicTieri <tieri.riccardo@gmail.com>
2023-11-28 18:53:20 +0100

    adjustment on recap version

--- recap-js-snake/main.js
+++ recap-js-snake/main.js
@@ -1,81 +1,169 @@
-console.log('main.js');
+
 const container = document.querySelector('.container');
-let direzione = 'left';
-let rowsNumber = 10;
-let columnsNumber = 10;
+let score = 0;
+let direzione = 'right';
+let columnsNumber = 20;
+let head = {};
+let body = [];
+let food = { point: 0 };
+let game;
+let game_run = true;
 
-createGrid();
 
-let position = Math.floor( Math.random() * (100) + 1 );
-console.log(position);
 
-document.querySelector('#cell-' + position).classList.add('active');
 
-function createGrid() {
-    for(let i = 0; i < 100; i++) {
-        let cell = document.createElement('div');
-        let position = Math.floor( Math.random() * (100) + 1 );
-        console.log(cell);
-        cell.classList.add('single-cell');
-        cell.id = 'cell-' + (i + 1);
-        container.appendChild(cell);
+document.querySelector('button#pause').addEventListener('click', () => {
+    if (game_run) {
+        clearInterval(game);
+        game_run = false;
+    } else {
+        start_game();
+        game_run = true;
     }
+})
+
+document.querySelector('button#reset').addEventListener('click', () => {
+    reset();
+    start_game();
+})
+
+document.querySelector('button#start').addEventListener('click', () => {
+    reset();
+    start_game();
+})
+
+
+
+
+// ===========================================FUNZIONI=========================================
+
+function reset(){
+    clearInterval(game);
+    container.innerHTML = '';
+    container.classList.remove('bg-black');
+    createGrid(container, columnsNumber);
+    food = { point: 0 };
+    body = [];
+    let position = Math.floor(Math.random() * (100) + 1);
+    head.point = position;
+    score = 0;
+    document.getElementById('score').innerHTML = score
+    game_run = true;
+};
+
+function start_game() {
+    game = setInterval(function () {
+        document.querySelector('#cell-' + head.point).classList.add('active');
+        document.querySelector('#cell-' + head.point).classList.remove('active');
+        body.forEach((element) => {
+            document.querySelector('#cell-' + element).classList.remove('active');
+        })
+
+        if (body.length) {
+            body.push(head.point);
+            body.shift();
+        };
+
+
+        for (let i = 0; i < body.length; i++) {
+            document.querySelector('#cell-' + body[i]).classList.add('active');
+        };
+
+        head.point = newPosition(direzione, head.point, columnsNumber);
+        document.querySelector('#cell-' + head.point).classList.add('active');
+
+
+        if (head.point == food.point) {
+            document.querySelector('#cell-' + food.point).classList.remove('food')
+            body.push(head.point);
+            food.point = 0;
+            score++;
+            document.getElementById('score').innerHTML = score;
+        };
+
+        if (food.point == 0) {
+            placeFood(1, 100, food);
+            document.querySelector('#cell-' + food.point).classList.add('food');
+        };
+
+        
+
+    }, 200);
 }
 
-setInterval(function() {
-    document.querySelector('#cell-' + position).classList.remove('active');
-    position = newPosition('right', position, columnsNumber);
-    document.querySelector('#cell-' + position).classList.add('active');
-}, 1000);
 
 function newPosition(direction, position, columns) {
 
-    switch(direction) {
+    switch (direction) {
 
         case 'right':
             position += 1;
-        break;
+            if (position % columns == 0) gameOver();
+            break;
 
         case 'left':
             position -= 1;
-        break;
+            if (position % columns == 1) gameOver();
+            break;
 
         case 'top':
             position -= columns;
-        break;
+            if (position < 0) gameOver();
+            break;
 
         case 'bottom':
             position += columns;
-        break;
+            if (position > columns*columns) gameOver();
+            break;
 
     }
 
     return position;
 
-}
+};
 
-document.addEventListener('keydown', function(event) {
-    switch(event.code) {
-        
+document.addEventListener('keydown', function (event) {
+    switch (event.code) {
+
         case "ArrowLeft":
-            direzione = 'left'
-        break;
+            if(direzione !== 'right') direzione = 'left';
+            break;
 
         case "ArrowUp":
-            direzione = 'top'
-        break;
+            if(direzione !== 'bottom') direzione = 'top'
+            break;
 
         case "ArrowDown":
-            direzione = 'bottom'
-        break;
+            if(direzione !== 'top') direzione = 'bottom'
+            break;
 
         case "ArrowRight":
-            direzione = 'right'   
-        break;
+            if(direzione !== 'left') direzione = 'right'
+            break;
 
     }
 
-})
+});
+
+function createGrid(container, columns) {
+    for (let i = 0; i < columns*columns; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('single-cell');
+        cell.id = 'cell-' + (i + 1);
+        cell.style.width = `calc(100% / ${columns} )`;
+        cell.style.height = `calc(100% / ${columns} )`;
+        container.appendChild(cell);
+    }
+};
+
+function placeFood(min, max, fruit) {
+    fruit.point = Math.floor(Math.random() * (max - min)) + min;
+};
+
+function gameOver() {
+    container.classList.add('bg-black');
+    clearInterval(game);
+};
 
 
 
